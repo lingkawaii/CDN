79a9fd13776073b813344a096f3f730a6b08251f
lingkawaii <252736954@qq.com>
2022-04-06 22:15:13 +0800

    2022.4.6 22:15

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,4 @@
 document.getElementById("footer").style.background = "transparent";
+document.querySelector("#site-title").addEventListener("click",function(){
+		document.querySelector("#page-header").attributes.style.value = "background-image: url(https://cdn.jsdelivr.net/gh/lingkawaii/PicGo/img/秋山澪.jpg)"
+})
