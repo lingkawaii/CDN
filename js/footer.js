0aeb20f541b2a816a53e58dbf0ad021eed81e96d
lingkawaii <252736954@qq.com>
2022-04-07 09:36:14 +0800

    2022.4.7 9:36

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,6 @@
 document.querySelector("#site-title").addEventListener("click",function(){
-		document.querySelector("#page-header").attributes.style.value = "background-image: url(https://cdn.jsdelivr.net/gh/lingkawaii/PicGo/img/秋山澪.jpg)"
+		var list_img = ["","https://cdn.jsdelivr.net/gh/lingkawaii/PicGo/img/Sea1of1clouds1云海.jpg", "https://cdn.jsdelivr.net/gh/lingkawaii/PicGo/img/REMEMBER.png", "https://cdn.jsdelivr.net/gh/lingkawaii/PicGo/img/Red1Temple.jpg", "https://cdn.jsdelivr.net/gh/lingkawaii/PicGo/img/azur1lane.jpg"]
+		var random = Math.ceil(Math.random()*4)
+		
+		document.querySelector("#page-header").attributes.style.value = "background-image: url(" + list_img[random] + ')'
 });
